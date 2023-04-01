4eca7a1a678a7064da2bb5b0fcb604473d82f685
Samuel Gouré <gmarcsamuel@gmail.com>
2023-04-01 04:20:05 +0000

    ajout de la première partie de to do list

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Forms from "./components/Forms";
+import TodosList from "./components/TodosList";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Forms
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
